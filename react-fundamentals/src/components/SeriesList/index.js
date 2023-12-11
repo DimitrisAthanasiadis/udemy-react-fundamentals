b32057498dda04e80fde2c56b564f97c9e4dd83b
Dimitris Athanasiadis <dimitris.athanasiadis@saphetor.com>
2023-12-11 12:56:24 +0200

    created SeriesListItem const

--- react-fundamentals/src/components/SeriesList/index.js
+++ react-fundamentals/src/components/SeriesList/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import "./index.css"
 
 
+const SeriesListItem = ({series}) => (
+    <li key={series.id}>
+        <h2>{series.name}</h2>
+        <p>{series.summary}</p>
+    </li>
+)
+
+
 const SeriesList = (props) => {
     return (
         <div>
             <ul className="series-list" >
                 {props.list.map(series => (
-                    <li key={series.id}>
-                        <h2>{series.name}</h2>
-                        <p>{series.summary}</p>
-                    </li>
+                    <SeriesListItem series={series} />
                 ))}
             </ul>
         </div>
