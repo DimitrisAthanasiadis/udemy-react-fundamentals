4997ee125741137be5f3de4bc998fd2135626b2b
Dimitris Athanasiadis <dimitris.athanasiadis@saphetor.com>
2023-12-18 13:53:52 +0200

    simple news fetching app using hooks

---
+++ react-tutorial/src/components/App/index.js
@@ -0,0 +1,14 @@
+import NewsForm from "../NewsForm";
+import "./index.css";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>News</h2>
+      <NewsForm/>
+    </div>
+  );
+}
+
+export default App;
